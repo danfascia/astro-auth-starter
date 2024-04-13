7a25fe5c1d17bcc2a447a9e4225c2d28eb339105
danfascia <danielfascia@gmail.com>
2024-04-14 00:03:43 +0100

    complete auth config and create User, Session tables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import htmx from 'astro-htmx';
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()]
+  integrations: [tailwind(), db(), htmx()],
+  output: 'hybrid',
+  vite: {
+    optimizeDeps: {
+      exclude: ["oslo"],
+      exclude: ["astro:db"],
+    }
+  },
 });
--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,24 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column, NOW } from 'astro:db';
+
+const User = defineTable({
+  columns: {
+    id: column.text({primaryKey: true}),
+    email: column.text({unique:true}),
+    hashed_password: column.text(),
+    createdAt: column.date({default: NOW}),
+  }
+})
+
+const Session = defineTable({
+  columns: {
+    id: column.text({primaryKey: true}),
+    userId: column.text({references: () => User.columns.id }),
+    expiresAt: column.date(),
+  }
+})
+
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {User, Session}
 });
